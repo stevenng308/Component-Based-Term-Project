fdd69522d0200381149adbe9738c1089f5381cc2
Steven Ng <stevenng308@gmail.com>
2013-11-23 17:04:22 -0500

    no message

--- classes/layout.php
+++ classes/layout.php
@@ -125,7 +125,7 @@ class Layout
 			}
 			
 		}
-		else
+		else if ($title == "Adopt")
 		{
 			if(!isset($_SESSION['sess_user_id']) || (trim($_SESSION['sess_user_id']) == '')) 
 			{
@@ -142,6 +142,32 @@ class Layout
 			{
 				$links = '<li><a href="index.php">Home</a></li>
 					<li class="active"><a href="adopt.php">Adopt</a></li>
+					<li><a href="email_newsletter_signup.php">Newsletter</a></li>
+					<li><a href="classes/logout.php">Sign-Out</a></li>
+					<li class="active"><a href="#" data-container="body" data-toggle="popover" 
+					data-placement="bottom" data-content="" data-original-title="" title="">
+					<span class="badge"><span class="simpleCart_quantity"></span></span> Items - <span class="simpleCart_total"></span>&nbsp;<span class="glyphicon glyphicon-shopping-cart"></span>
+					</a></li>';
+			}
+			
+		}
+		else
+		{
+			if(!isset($_SESSION['sess_user_id']) || (trim($_SESSION['sess_user_id']) == '')) 
+			{
+				$links = '<li><a href="index.php">Home</a></li>
+					<li><a href="adopt.php">Adopt</a></li>
+					<li><a href="email_newsletter_signup.php">Newsletter</a></li>
+					<li><a href="login.php">Sign-In</a></li>
+					<li class="active"><a href="#" data-container="body" data-toggle="popover" 
+					data-placement="bottom" data-content="" data-original-title="" title="">
+					<span class="badge"><span class="simpleCart_quantity"></span></span> Items - <span class="simpleCart_total"></span>&nbsp;<span class="glyphicon glyphicon-shopping-cart"></span>
+					</a></li>';
+			}
+			else
+			{
+				$links = '<li><a href="index.php">Home</a></li>
+					<li><a href="adopt.php">Adopt</a></li>
 					<li><a href="email_newsletter_signup.php">Newsletter</a></li>
 					<li><a href="classes/logout.php">Sign-Out</a></li>
 					<li class="active"><a href="#" data-container="body" data-toggle="popover" 
